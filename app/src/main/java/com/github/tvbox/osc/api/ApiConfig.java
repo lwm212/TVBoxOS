057a16397f69c5af4cf0642d8c1679d77ddf20d8
lwm212 <106297942+lwm212@users.noreply.github.com>
2023-01-29 16:18:57 +0800

    Update ApiConfig.java

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -127,7 +127,7 @@ public class ApiConfig {
     }
 
     public void loadConfig(boolean useCache, LoadConfigCallback callback, Activity activity) {
-        String apiUrl = Hawk.get(HawkConfig.API_URL, "https://notabug.org/lwm212/boxx/raw/master/khjk/2302.json");
+        String apiUrl = Hawk.get(HawkConfig.API_URL, "https://cdn.jsdelivr.net/gh/lwm212/JK@main/JK/wd.json");
         if (apiUrl.isEmpty()) {
             callback.error("-1");
             return;
