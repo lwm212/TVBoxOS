2188395a39e42e540b5179fa1ffb5be643bab410
lwm212 <106297942+lwm212@users.noreply.github.com>
2023-08-28 14:41:08 +0800

    Update ApiConfig.java

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -127,7 +127,7 @@ public class ApiConfig {
     }
 
     public void loadConfig(boolean useCache, LoadConfigCallback callback, Activity activity) {
-        String apiUrl = Hawk.get(HawkConfig.API_URL, "https://jihulab.com/lwm212/box/-/raw/main/PZ/XYwangluo.json");
+        String apiUrl = Hawk.get(HawkConfig.API_URL, "https://jihulab.com/lwm212/box/-/raw/main/PZ/XY53.json");
         if (apiUrl.isEmpty()) {
             callback.error("-1");
             return;
